757837bb205e401c9d5ad04c3ec123996ef4dff8
hanaachahbar <hanaa.chahbar@ensia.edu.dz>
2025-10-17 10:54:55 +0100

    adjust links of the app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AyaMouffok from "./pages/Aya-mouffok";
+import HanaaChahbar from "./pages/Hanaa-chahbar";
+import IlafeNamra from "./pages/Ilafe-namra-hamdat";
+import MohamedSaad from "./pages/Mohamed-alamin-saad";
+import DanilOuakli from "./pages/Danil-ouakili";
+import AymenZahzouh from "./pages/Aymen-zahzouh";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Aya-mouffok" element={<AyaMouffok />} />
+        <Route path="/Hanaa-chahbar" element={<HanaaChahbar />} />
+        <Route path="/Ilafe-namra-hamdat" element={<IlafeNamra />} />
+        <Route path="/Mohamed-alamin-saad" element={<MohamedSaad />} />
+        <Route path="/Danil-ouakli" element={<DanilOuakli />} />
+        <Route path="/Aymen-zahzouh" element={<AymenZahzouh />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
